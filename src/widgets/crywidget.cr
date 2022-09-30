c49d1d8b30d9955df976c1124064a4386f0309ac
Andriamanitra <mikko77792@gmail.com>
2022-09-30 13:40:36 +0300

    move CryWidget inside the CryPaint module

--- src/widgets/crywidget.cr
+++ src/widgets/crywidget.cr
@@ -1,6 +1,8 @@
-abstract class CryWidget
-  abstract def show
-  abstract def name_in_menu : String
-  abstract def toggle_visibility
-  abstract def visible? : Bool
+module CryPaint
+  abstract class CryWidget
+    abstract def show
+    abstract def name_in_menu : String
+    abstract def toggle_visibility
+    abstract def visible? : Bool
+  end
 end
